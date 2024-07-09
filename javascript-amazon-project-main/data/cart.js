9027feec507e7a2eb349fee5a200e6db72457408
Rayann18 <gderrick768@gmail.com>
2024-07-09 13:34:52 +0300

    save cart into local storage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,13 +1,20 @@
 // saved data 
-export let cart = [
-  {
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart) {
+  cart = [{
     productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6' ,
     quantity : 2
-  } , {
+  }, {
     productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity : 1
   }
  ];
+}
+
+ function saveTostorage(){
+  localStorage.setItem('cart' , JSON.stringify(cart));
+ }
 
   export function addTocart(productId) {
   let matchingItem;
@@ -26,7 +33,10 @@ export let cart = [
       quantity : 1
     })
   }
+  saveTostorage();
 }
+
+
 
 export function removeFromCart(productId){ // removes product id from cart
 const newCart = [];
@@ -38,4 +48,7 @@ cart.forEach( (cartItem) =>{
 })
 
 cart  = newCart; 
+
+saveTostorage();
 }
+
