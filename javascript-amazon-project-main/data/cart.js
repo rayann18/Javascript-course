8f510c5552c821d81e455b80ff7276047045b93c
Rayann18 <gderrick768@gmail.com>
2024-07-09 12:41:48 +0300

    delete products from cart when clicking delete

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,5 +1,5 @@
 // saved data 
-export const cart = [
+export let cart = [
   {
     productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6' ,
     quantity : 2
@@ -26,4 +26,16 @@ export const cart = [
       quantity : 1
     })
   }
+}
+
+export function removeFromCart(productId){ // removes product id from cart
+const newCart = [];
+
+cart.forEach( (cartItem) =>{
+  if(cartItem.productId !== productId){
+    newCart.push(cartItem);
+  }
+})
+
+cart  = newCart; 
 }
