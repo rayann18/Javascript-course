c68e981910497e911cd0dcc3bd8d8f6636b91a00
Rayann18 <gderrick768@gmail.com>
2024-07-17 16:29:54 +0300

    learn more details about classes and 'this'

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -58,6 +58,38 @@ export function getProduct (productId){
   return matchingProduct
 }
 
+/*
+// built in class methods
+const date  = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+console.log(this);
+
+const object2 = {
+  a : 2,
+  b : this.a
+}
+*/
+
+
+/*
+function logThis(){
+  console.log(this)
+}
+logThis();
+logThis.call('hello');
+
+this
+const object3 = {
+  method : () => {
+    console.log(this)
+  }
+}
+object3.method()
+*/
 
 export const products = [
 
